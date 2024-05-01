e27dfafcf635f28d27dd49435d83763813ed4c1e
Pankaj Soni <soni.pankajkumar@gmail.com>
2024-05-01 22:31:27 +0530

    Added more configuration for test and python package creation

--- template.py
+++ template.py
@@ -6,7 +6,7 @@ from pathlib import Path
 project_name = input("Enter Project Name without any space...  ")
 list_of_dir_and_files = [
     f"{project_name}_project/{project_name}_model/__init__.py",
-    f"{project_name}_project/{project_name}_model/VERSION.txt",
+    f"{project_name}_project/{project_name}_model/VERSION",
     f"{project_name}_project/{project_name}_model/config.yml",
     f"{project_name}_project/{project_name}_model/pipeline.py",
     f"{project_name}_project/{project_name}_model/predict_pipeline.py",
@@ -19,7 +19,16 @@ list_of_dir_and_files = [
     f"{project_name}_project/{project_name}_model/processing/features.py",
     f"{project_name}_project/{project_name}_model/processing/validation.py",
     f"{project_name}_project/{project_name}_model/trained_models/__init__.py",
-    f"{project_name}_project/requirements/requirements.txt"
+    f"{project_name}_project/requirements/requirements.txt",
+    f"{project_name}_project/requirements/test_requirements.txt",
+    f"{project_name}_project/tests/__init__.py",
+    f"{project_name}_project/tests/conftest.py",
+    f"{project_name}_project/tests/test_features.py",
+    f"{project_name}_project/tests/test_predictions.py",
+    f"{project_name}_project/MANIFEST.in",
+    f"{project_name}_project/mypy.ini",
+    f"{project_name}_project/pyproject.toml",
+    f"{project_name}_project/setup.py"
 ]
 
 for filepath in list_of_dir_and_files:
